97ac0e72e5b93b4d5e78244058b2fc7d7d053159
Ooz47 <yannick.abaul@gmail.com>
2025-04-03 16:25:19 -0400

    Feat: Responsive

--- web/themes/custom/interventions_democratiques/js/interventions-democratiques.js
+++ web/themes/custom/interventions_democratiques/js/interventions-democratiques.js
@@ -10,6 +10,17 @@
     attach (context, settings) {
 
       console.log('It works!');
+      // document.addEventListener('DOMContentLoaded', function () {
+      //   setTimeout(function () {
+      //     console.log('It sds!');
+      //     document.querySelectorAll('.splide').forEach(function (el) {
+      //       if (el.splide) {
+      //         el.splide.refresh();
+      //       }
+      //     });
+      //   }, 150);
+      // });
+      
 
     }
   };
